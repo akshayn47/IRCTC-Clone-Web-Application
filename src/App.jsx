596a2c35e34773147bd6e2c9bf016e0e57a4a575
akshay <55614534+akshayn47@users.noreply.github.com>
2025-11-02 00:00:48 +0530

    Added all project files

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import BookingPage from "./pages/BookingPage";
+import ContactPage from "./pages/ContactPage";
+import LoginModal from "./pages/LoginModal";
+import TrainCarousel from "./components/TrainCarousel";
+import styles from "./styles/App.module.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+import TrainSearchResults from "./pages/TrainSearchResults";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthState } from "./redux/auth/authSlice";
+import { observeAuthState } from "./services/authServices";
+import TrainDetails from "./pages/TrainDetails";
+import BookingConfirmation from "./pages/BookingConfirmationPage";
+import MyBookings from "./pages/MyBookings";
+
+// Component to conditionally render content based on route
+const RouteContentManager = () => {
+  return (
+    <div className={styles.mainContent}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <TrainCarousel />
+            </>
+          }
+        />
+        <Route path="/train-search" element={<TrainSearchResults />} />
+        <Route path="/train-details/:train_number" element={<TrainDetails />} />
+        <Route path="/booking-confirmation" element={<BookingConfirmation/>} />
+        <Route
+          path="/booking"
+          element={
+            <ProtectedRoute>
+              <BookingPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route 
+          path="/my-bookings" 
+          element={
+            <ProtectedRoute>
+              <MyBookings />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route
+          path="/login"
+          element={
+            <LoginModal
+              isOpen={true}
+              onClose={() => window.history.back()}
+              onLogin={() => {}}
+              switchToRegister={() => {}}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
+
+function App() {
+  const dispatch = useDispatch();
+  const { isInitialized } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    observeAuthState((user) => {
+      dispatch(setAuthState(user));
+    });
+  }, [dispatch]);
+
+  if (!isInitialized) {
+    return <>Loading....</>;
+  }
+
+  return (
+    <Router>
+      <div className={styles.app}>
+        <Navbar />
+        <RouteContentManager />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
